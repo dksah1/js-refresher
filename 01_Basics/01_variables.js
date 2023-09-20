bbf839dd7fe0a065b36ebce52405f4967b2f2031
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-09-20 16:36:56 +0000

    add changes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail ="hfdhasd@gmail.com"
+var accountPassword="12343"
+accountCity = "jaipur"
+
+// accountId = 2  not allowed
+
+
+accountEmail ="hdhs@gmail.com"
+accountPassword ="4567"
+accountCity  = "Dharan"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
