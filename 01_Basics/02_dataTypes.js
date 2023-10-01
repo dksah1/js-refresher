d46bef73f7404bd7664bf9d90d055f2df541fcbd
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-01 18:43:58 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+// alert(3+5) //we are using node js not browser
+
+// console.log( 3 + 3);
+//
+
+//premitive data types
+
+// number => 2 to power 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=> iS a standalone value
+// undefined=> 
+// symbol=> unique
+ 
+// object
+let a = "sita"
+console.log(typeof a)
+
+console.log(typeof null); // output should be object
+console.log(typeof undefined); // output should be undefined
+
