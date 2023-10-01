661a60c75b19313c2c30e622c7555bfcde3e8c59
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-01 19:13:13 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+// conversion of datatypes
+
+// let score = "nsdf";
+// let score = undefined
+// let score = null
+// let score = true //1
+let score = 34 //34
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log( (valueInNumber));
+
+
+let isLoggedIn=""
+let bool = Boolean(isLoggedIn)
+console.log(bool)
+// 1 => true; 0 =>false
+// "" >false
+// "bob" => true
+
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
+
