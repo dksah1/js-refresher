0191612e77148b027870c3be40b8cbe6b4c0f36e
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-01 19:51:05 +0000

    comparision operator

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,17 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+// this type of comparision can take you in to the confusion
+// console.log(null >0);
+// console.log(null == 0);
+// console.log(null >=0);
+
+// === strict check 
+
+console.log("2" === 2);
