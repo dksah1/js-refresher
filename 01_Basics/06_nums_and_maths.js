2947b27ed4298252bb6a97932039f46c172043fb
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-02 09:37:40 +0000

    maths and number

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+//  const score = 499
+//  console.log(score);
+//  const balance = new Number(500)
+//  console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+// const otherNumber =33.274
+
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// // maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.2)); // ceilling value
+// console.log(Math.floor(4.9)); // floor value
+// console.log(Math.sqrt(4)); // floor value
+// console.log(Math.max(4)); // floor value
+// console.log(Math.max(4,34,45,56)); // floor value
+// console.log(Math.min(4,34,5,34,45)); // floor value
+
+console.log(Math.random());
+console.log((Math.random()*10 )+ 1);
+console.log(Math.floor(Math.random()*10 )+ 1);
+
+const min = 10
+const max =20
+console.log(Math.floor(Math.random() * (max- min + 1)) + min )
+
