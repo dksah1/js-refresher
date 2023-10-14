8b87572e767ee7865247227267128aec54a52413
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-14 16:31:02 +0000

    array method

--- 01_Basics/01_array.js
+++ 01_Basics/01_array.js
@@ -6,4 +6,17 @@ const myHeros = ["shaktiman","naagraj"]
 
 const myArr2 = new Array(1,2,3,4)
 
-console.log(myArr[0])
+console.log(myArr[0]);
+
+
+// array method
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9) //add values at the first index only
+// myArr.shift() //delete the values from the first index only
+
+// console.log(myArr.includes(5)); //this returns the boolean values 
+// console.log(myArr.indexOf(2)); give the index no. of an element of an array 
+console.log(myArr);
