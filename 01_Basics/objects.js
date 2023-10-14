6f830e49549d0f4e732407f17f83c731c4f85d52
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-14 19:45:19 +0000

    obejects in js

---
+++ 01_Basics/objects.js
@@ -0,0 +1,19 @@
+// singleton
+// Object.create
+
+// object literals
+
+const JsUser = {
+    name :"Hitesh",
+    age:18,
+    location:"jaipur",
+    email:"hitesh@gmail.com",
+    isLoggedin:false,
+    lastLoggedinDays:["Monday","Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+
+
+
