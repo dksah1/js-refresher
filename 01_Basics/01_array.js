dac278cf3d1777f0efc464dba32cb8a1ca3fb25f
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-14 19:18:56 +0000

    array methods

--- 01_Basics/01_array.js
+++ 01_Basics/01_array.js
@@ -9,7 +9,7 @@ const myArr2 = new Array(1,2,3,4)
 console.log(myArr[0]);
 
 
-// array method
+// array method part 1
 
 // myArr.push(6)
 // myArr.push(7)
@@ -25,6 +25,8 @@ console.log(myArr);
 // console.log(newArr);
 // console.log(myArr);
 
+// array method part2 
+
 
 // slice,splice
 
@@ -37,3 +39,37 @@ console.log("B",myArr);
 const myn2= myArr.splice(1,3)
 console.log("C",myArr);
 console.log(myn2);
+
+
+// array method part3
+
+// const marvel_heros = ["thor","Ironman","spiderman"]
+// const dc_heros= ["superman","flash","batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+
+// const all_new_heros = [...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+ 
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(...real_another_array,"end");
+
+
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name:"Hitesh"})) //intresting
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
