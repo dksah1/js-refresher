8d4e76faba8a90cae8b7e7ec5ff5d2e14981fc08
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-10-19 11:26:21 +0000

    functions in js

---
+++ 01_Basics/01_functions.js
@@ -0,0 +1,27 @@
+function calculateCartPrice(val1,val2,...num1){
+return num1
+}
+// console.log(calculateCartPrice(200,300,500,2000))
+
+// const user = {
+//     username:"hitesh",
+//     price:199
+// }
+function handleObject(anyobject){
+console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price:399
+})
+
+const mynewarray = [200,400,100,600]
+
+function returnsecondvalue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnsecondvalue(mynewarray));
+console.log(returnsecondvalue([200,400,100,600]));
