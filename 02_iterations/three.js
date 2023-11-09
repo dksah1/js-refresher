4b1ee2eb12ce5979a8da0a9ac4363d7db16cef15
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-11-09 17:54:10 +0000

    for of loop and use of for of loop using map method

---
+++ 02_iterations/three.js
@@ -0,0 +1,24 @@
+// for of loops
+
+// ["","",""]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "hello world"
+for (const greet of greetings) {
+    // console.log(`each char is ${greet} `);
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of america")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+console.log(map);
