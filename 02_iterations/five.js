c694e3b3eed3a1849cdb62c9e20ccf57538af0a0
dksah1 <135329193+dksah1@users.noreply.github.com>
2023-12-29 19:11:17 +0000

    reduce

--- 02_iterations/five.js
+++ 02_iterations/five.js
@@ -2,5 +2,10 @@ const mynums = [1,2,3,4,5,6,7,8,9,10]
 
 // const newnums = mynums.map((num)=>num +10)
 
-const newnums = mynums.map((num)=>num *10 ).map((num)=> num+2)
-console.log(newnums);
+// const newnums = mynums.map((num)=>num *10 ).map((num)=> num+2)
+// console.log(newnums);
+
+const myTotal = mynums.reduce((acc,currval)=>{
+    console.log(`acc:${acc} currval : ${currval}`);
+    return acc + currval
+}, 0)
